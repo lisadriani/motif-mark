a73ed1d9b0614a6c240631d6a22ca0d101df6a93
Lisa Adriani <ladriani@uoregon.edu>
2023-02-23 14:02:11 -0800

    updating after base code is finished with first run of png

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -5,6 +5,7 @@ import cairo
 import math
 import argparse
 from itertools import product
+from random import sample
 
 def get_args():
     parser = argparse.ArgumentParser(description="This code will take a fasta file (with exons capitalized and introns lowercase), and a file of motifs and output a figure showing the motifs on an png.")
@@ -13,11 +14,13 @@ def get_args():
     return parser.parse_args()
 	
 args = get_args()
-
 fasta_file = args.fasta
 match = re.match(re.compile("^(.*)\..*"), args.fasta) #find the name of the file before the "."
-file_name =  match.group(1) 
+file_name =  match.group(1)
 motif_file = args.motifs
+
+random_numbers = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+
 
 def validate_base_seq(DNA:str, RNA_flag: bool=False):
     '''This will validate if a string that is given is a base sequence, returning True or False'''
@@ -45,28 +48,50 @@ def oneline_fasta(file):
     o.write(str(build_seq))
     return(o)
 
-def draw_figure(motif,fasta, file_name):
+def draw_figure(motif_list,fasta_list, color_dict, file_name):
 
-
-    width,height =  int(fasta.length), 100
+    width,height =  1000, ((len(fasta_list)*50+100))
 
     surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width,height)
     context = cairo.Context(surface)
 
-    context.set_line_width(1)
-    ##create the intron line##
-    context.move_to(0,50) #(x,y)
-    context.line_to(int(fasta.length)),50)
-    context.stroke()
+    ii=40
+    line_value = 50
+    for fastaseq in fasta_list:
+        #print(fasta)
+        context.set_line_width(1)
+        ##create the intron line##
+        context.move_to(0,(line_value)) #(x,y)
+        context.line_to(fastaseq.length,(line_value))
+        context.set_source_rgb(0,0,0)
+        context.stroke()
 
-    fasta.get_exon(fasta)
-    ##creates a box precisely in the center. This will have to be edited depending on how many exons there are##
-    context.rectangle(100,25,200,50)    #(x0,y0,x1,y1)
-    context.fill()
+        exons = fastaseq.find_exon(fastaseq.fasta)
+        for start,end in exons.items():
+            context.rectangle(start,ii, (end-start), 20)    #(x0,y0,x1,y1)
+            context.set_source_rgb(0,0,0)
+            context.fill()
+        for motifseq in motif_list:
+            motif_dict = motifseq.find_motif(fastaseq.fasta)
+            for motif,integers in motif_dict.items():
+                color1= color_dict[motif][0]
+                color2= color_dict[motif][1]
+                color3= color_dict[motif][2]
+                context.rectangle(integers[1],ii, (integers[0]-integers[1]), 20)    #(x0,y0,x1,y1)
+                context.set_source_rgb(color1,color2,color3)
+                #context.rectangle()
+                context.fill()
+        ii+= 50
+        line_value += 50
 
+        ##make a legend
+    # context.rectangle(20,ii, 960, 50)    #(x0,y0,x1,y1)
+    # context.set_source_rgb(0,0,0)
+    # context.fill()
 
 
     surface.write_to_png(file_name+".png")
+    return 
 
 class Motif:
 
@@ -74,23 +99,35 @@ class Motif:
         '''This is a motif'''
 
         ##the data##
+
         self.motif = the_seq
         self.length = len(the_seq)
-        self.ambiguous_motifs = []
+        self.ambiguous_motif = self.ambiguous(self)
 
 
     ## the methods ##
 
-    # def the_length(self, the_seq):
-    #     self.length = len(the_seq)
+    def find_motif(self, fasta):
+        regex = self.ambiguous_motif
+        motif_dict = dict()
+        match = str()
+        color_dict = dict()
+        for match in re.finditer(rf"{regex}", str(fasta)):
+            #print(match.group())
+            if match != None:
+                motif_dict[match.group()]=(match.end(),match.start())
+                #color_dict[match.group()] = sample(random_numbers, 3)
+            #if len(motif_dict)>1:
+        return  motif_dict
 
-    def find_motif(self, Fasta):
-        pass
 
     def ambiguous(self,motif):
-        ambiguous_bases = {"Y":["C","T","U"], "y":["c","t","u"], "g":["g"], "t":["t"],"a":["a"], "c":["c"],"u":["u"], "G":["G"], "T":["T"],"C":["C"],"U":["U"], "A":["A"]}
-        self.ambiguous_motifs = list(map("".join, product(*map(ambiguous_bases.get, motif))))
-        return list(map("".join, product(*map(ambiguous_bases.get, motif))))
+        ambiguous_bases = {"Y":"[CTUctu]", "y":"[ctu]", "G":"[Gg]", "T":"[Tt]","A":"[Aa]", "C":"[Cc]","U":"[Uu]", "G":"[Gg]", "T":"[TtUu]", "W":"[AaTtUu]","S":"[CcGg]", "M":"[AaCc]", "K":"[GgTtUu]", "R": "[AaGg]", "B":"[CcGgTtUu]", "D":"[AaGgTtUu]", "H":"[AaCcTtUu]", "V":"[AaCcGg]", "N":"[AaCcGgTtUu]"}
+        motif = self.motif.upper()
+        finder = str()
+        for letter in motif:
+            finder+= ambiguous_bases[letter] 
+        return finder
 
 class Fasta: 
 
@@ -108,18 +145,28 @@ class Fasta:
     #     self.length = len(the_seq)
 
     def find_exon(self,fasta):
-        #prior = len(fasta)
-        #fasta = re.split(r"(A-Z]+", fasta)
-        #after = len(fasta)
-        the_uppers = []
+        the_start = []
+        the_end = []
+        exons = dict()
+        ii = 0
+        iii = 0
         for i in range(0,len(fasta)):
-            if fasta[i] is.upper():
-                the_uppers.append(i)
+            if fasta[i].isupper()== True:
+                iii = 0
+                if i-ii not in the_start:
+                    the_start.append(i)
+                ii +=1
+            if the_start != []:
+                if  fasta[i].isupper()== False:
+                    if i - iii not in the_end:
+                        the_end.append(i)
+                        ii = 0
+                    iii +=1
+        for end in the_end:
+            for start in the_start:
+                exons[start] = end
 
-        return fasta
-
-    def find_intron(self,fasta):
-        pass
+        return exons
 
     def find_motif(self,fasta:list,motif:list):
         pass
@@ -156,18 +203,60 @@ def classify(fasta,motif_file):
 
 
 
+
 oneline = oneline_fasta(fasta_file)
 fastas,motifs = classify(oneline,args.motifs) #returns a list of classes. 
 
+for fastaseq in fastas:
+    for motifseq in motifs: 
+        ambig = motifseq.ambiguous(motifseq.motif)
+       # print(motifseq.ambiguous_motif)
 
-for motifseq in motifs: 
-    ambig = motifseq.ambiguous(motifseq.motif)
-    #print(motifseq.ambiguous_motifs)
+for fastaseq in fastas:
+    pass
+    exons = fastaseq.find_exon(fastaseq.fasta)
+    #print(exons)
+    #print(fastaseq.length)
+
+
+color_dict=dict()
+for fastaseq in fastas:
+    for motifseq in motifs:
+        #print(motifseq.ambiguous_motif)
+        motif_dict = motifseq.find_motif(fastaseq.fasta)
+
+        #print(motif_dict)
+        #print(color_dict)
+
+        #draw_figure(motifseq,fastaseq,file_name)
 
 
 
+    ###if it's based off of every individual motif ###
+        for motif in motif_dict:
+
+            if motif in color_dict:
+                pass
+            if motif not in color_dict:
+                #samps = sample(random_numbers,1)
+                #print(samps)
+                color_dict[motif]= sample(random_numbers, 3)
 
 
+    #### if its based off of the motif file:  ####
+    # for motif in motif_file:
+    #     if motif in color_dict:
+    #         pass
+    #     if motif not in color_dict:
+    #         color_dict[motif] = sample(random_numbers, 3)
+
+draw_figure(motifs,fastas,color_dict,file_name)
+
+# for fastaseq in fastas:
+#     for motifseq in motifs:
+#         #print(motifseq.ambiguous_motif)
+#         motif_dict = motifseq.find_motif(fastaseq.fasta)
+#         print(motif_dict)
 
 
 
@@ -178,3 +267,22 @@ for motifseq in motifs:
 #     print(fastaseq.fasta)
 #     print(fastaseq.length)
 #     print(fastaseq.header)
+
+
+        # for motifseq in motif_list:
+        #     motif_dict = motifseq.find_motif(fasta)
+        #     if motif_dict != None:
+        #         for index, start, end in enumerate(motif_dict.items()):
+        #             index = index/.1
+        #             context.set_source_rgb(0.9, 0.1, 0.1)
+        #             context.rectangle(start,ii,(end-start),20)
+        #             context.fill()
+
+
+                            # for start,end in motif_dict.items():
+        #     context.set_line_width(1)
+        #     ##create the intron line##
+        #     context.move_to(0,(line_value)) #(x,y)
+        #     context.line_to(fasta.length,(line_value))
+        #     context.stroke()
+        #     pass
